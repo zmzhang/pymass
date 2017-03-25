bcc7ab0f2ccf3021d0b3db69c9c322f12891e0e9
zmzhang <zhangzhimin.csu@gmail.com>
2017-03-25 22:33:43 +0800

    move thirdparty to parent dir

--- python/test.py
+++ python/test.py
@@ -1,30 +1,45 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Oct 08 23:27:50 2015
+Created on Sat Mar 25 22:16:48 2017
 
-@author: humblercoder
+@author: zmzhang
 """
+
+
 import sys
-import _pymass
 from _pymass import MZXML
 from matplotlib.pylab import plot, show, figure
-#_pymass.testMZXML()
 
 
+def tic():
+    #Homemade version of matlab tic and toc functions
+    import time
+    global startTime_for_tictoc
+    startTime_for_tictoc = time.time()
 
-mzfile=u"D:/workspace/pymass/python/标2-方法5-正负离子_Seg1Ev1.mzXML"
+def toc():
+    import time
+    if 'startTime_for_tictoc' in globals():
+        print("Elapsed time is " + str(time.time() - startTime_for_tictoc) + " seconds.")
+    else:
+        print("Toc: start time not set")
 
-#_pymass.testMZXML()
+
+mzfile=u"mixture_bsa300fmol_n3.mzXML"
 
 mz1=MZXML()
+
+tic()
 mz1.parseFile(mzfile.encode(sys.getfilesystemencoding()))
+toc()
+
+
+
 rt=mz1.getRT()
 bic=mz1.getBIC()
 tic=mz1.getTIC()
 plot(rt,bic,'r')
 plot(rt,tic,'g')
-
-
 
 mz=mz1.getMS(0)
 val=mz1.getVal(0)
@@ -32,4 +47,3 @@ val=mz1.getVal(0)
 figure()
 plot(mz,val)
 show()
-
