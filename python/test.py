2850d58158e70d5bd7c1fd2677f742bb7f1b26fd
zmzhang <zhangzhimin.csu@gmail.com>
2017-05-24 20:26:59 +0800

    Remove redundant functions in Python

--- python/test.py
+++ python/test.py
@@ -55,122 +55,6 @@ def mzdata2mzxml(path, converter = 'C:/Program Files/OpenMS/bin/FileConverter.ex
             subprocess.Popen([converter, '-in', file_in, '-out', file_out])
 
 
-def find_closest(A, target):
-    # A must be sorted
-    if len(A) == 0:
-        return []
-    if len(A) == 1:
-        return 0
-    idx = A.searchsorted(target)
-    idx = np.clip(idx, 1, len(A) - 1)
-    left = A[idx - 1]
-    right = A[idx]
-    idx -= target - left < right - target
-    return idx.tolist()
-
-def find_idx(rg, rt, mz, threshold):
-    rt =rg[find_closest(rg[:,0], rt)][0]
-    idx_l = np.searchsorted(rg[:,0], rt, side='left')
-    idx_u = np.searchsorted(rg[:,0], rt, side='right')
-    idx = find_closest(rg[idx_l:idx_u,1], mz) + idx_l
-    if rg[idx,1] > mz-threshold and rg[idx,1] < mz+threshold:
-        return idx
-    else:
-        return -1
-
-
-def pic_seeds(rmv_sort, idx, b_inc, mz_tol = 0.5):
-    seed_set = SortedSet(key=lambda val: val[1])
-    for i in range(idx):
-        if b_inc[i] == True:
-            continue
-        idx = seed_set.bisect_left(tuple(rmv_sort[i])) 
-        if len(seed_set) == 0:
-            seed_set.add(tuple(rmv_sort[i]))
-        else:
-            if idx == 0:
-                if seed_set[0][1] - rmv_sort[i][1] > mz_tol:
-                    seed_set.add(tuple(rmv_sort[i]))
-            elif idx == len(seed_set):
-                if rmv_sort[i][1] - seed_set[-1][1] > mz_tol:
-                    seed_set.add(tuple(rmv_sort[i]))
-            else:
-                if seed_set[idx][1] - rmv_sort[i][1] >= mz_tol and \
-                   rmv_sort[i][1] - seed_set[idx-1][1] >= mz_tol:
-                    seed_set.add(tuple(rmv_sort[i]))   
-    seeds = np.array(seed_set) 
-    if seeds.shape[0] >0:
-        return seeds[seeds[:,2].argsort()[::-1],:]
-    else:
-        return []
-
-def FPIC(lcms, seed, rt_width, mz_width, b_plot=True):
-
-    pic_ids = []
-    if b_plot:
-        stds=[]
-    rg   = get_region(seed, lcms, rt_width, mz_width)
-    rtm = np.mean(np.diff(lcms.getRT().T))
-    pic_ids.append(find_idx(rg, seed[0], seed[1], sys.float_info.max))
-    b_left=True
-    b_right=True
-    threshold = sys.float_info.max
-    for i in range(0,rt_width):
-        if b_plot:
-            stds.append(np.std(rg[pic_ids,1]))
-        if len(pic_ids) == 5:  
-            threshold = 10 * np.std(rg[pic_ids,1])
-        if b_left:
-            rt_left  = rg[pic_ids[0]][0] - rtm
-            idx_left = find_idx(rg, rt_left, seed[1], threshold) 
-            if idx_left != -1:
-                pic_ids.insert(0, idx_left)
-            else:
-                b_left = False
-        
-        if b_right:
-            rt_right = rg[pic_ids[-1]][0] + rtm 
-            idx_r = find_idx(rg, rt_right, seed[1], threshold)
-            if idx_r != -1:
-                pic_ids.append(idx_r)
-            else:
-                b_right=False
-        
-        if b_left == False and b_right == False:
-            break;
-    pic = np.array([rg[i] for i in pic_ids])
-    
-    if b_plot:
-        plot_region(rg, seed[0], seed[1])  
-        plot_region(pic, seed[0], seed[1])
-        figure()
-        plot(pic[:,0], pic[:,2])
-        figure()
-        plot(stds)
-
-    return pic
-
-def FPICs(lcms, min_peak=300.0, rt_width=100, mz_width=0.5):
-    rmv      = lcms.getAll()
-    ids      = rmv.shape[0] - rmv[::-1,2].argsort(kind='mergesort')[::-1] -1
-    rmv_sort = rmv[ids,:]
-    rids     = ids.argsort(kind='mergesort')
-    idx      = rmv_sort.shape[0] - rmv_sort[::-1,2].searchsorted(min_peak)
-    b_inc    = np.full((rmv.shape[0],), False, dtype=bool)
-    
-    pics=[]
-    
-    while not(np.all(b_inc[0:idx])):
-        print(np.sum(b_inc[0:idx])/idx)
-        seeds = pic_seeds(rmv_sort, idx, b_inc)
-        
-        for seed in seeds:
-            pic = FPIC(lcms, seed, rt_width, mz_width, False)
-            b_inc[rids[pic[:,3].astype(int)]] = True
-            pics.append(pic)
-
-    return pics
-
 def pics_id(pics, n):
     pic_ids=[]
     for pic in pics:
@@ -182,39 +66,11 @@ def pics_id(pics, n):
     return pic_ids_sort
 
 #mzdata2mzxml('F:/resources/MTBLS188/study files/')
-#mzfile=u"mixture_bsa300fmol_n3.mzXML"
 mzfile=u"MM14_20um.mzxml"
-
 parser=mzXMLParser()
-
-
 lcms = parser.parseFile(mzfile.encode(sys.getfilesystemencoding()))
-
-
-
-
-rts=lcms.getRT()
-bic=lcms.getBIC()
-tics=lcms.getTIC()
-#plot(rts,bic,'r')
-#plot(rts,tics,'g')
-
-pics_p = FPICs(lcms)
 pics_c = pm.FPICs(lcms, 300.0, 100.0, 0.5)
 
-
-n = max(len(pics_c), len(pics_p))
-ids_p = pics_id(pics_p, n)
-ids_c = pics_id(pics_c, n)
-
-a = np.array([ids_p, ids_c]).T
-
-pic_id =   3355          
-l_c = [i for i, p in enumerate(pics_c) if p[p[:,2].argmax(), 3] == pic_id]
-l_p = [i for i, p in enumerate(pics_p) if p[p[:,2].argmax(), 3] == pic_id]
-
-ps_c = [pics_c[i] for i in l_c]
-ps_p = [pics_p[i] for i in l_p]
 
 
 figure()
